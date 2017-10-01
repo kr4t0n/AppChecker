f87a6a53bd97f75b1ad51baaf15d512de8f4482b
Kr4t0n <kyle.yang1995@gmail.com>
2017-10-01 16:50:04 +0800

    Constructing a class to the function

--- AppChecker.py
+++ AppChecker.py
@@ -4,8 +4,6 @@ import requests
 from functools import wraps
 from bs4 import BeautifulSoup
 
-app = ['Alfred 3']
-
 
 def log(debug=True):
     if debug:
@@ -22,30 +20,66 @@ def log(debug=True):
     return _log
 
 
-@log(False)
-def load_page_html(url):
-    ''' Obtain the page's HTML content '''
-    return requests.get(url).content
+class AppChecker(object):
+
+    def __init__(self):
+        self.appDic = {'Alfred 3':
+                       {'Name': 'Alfred 3',
+                        'URL': ('https://www.macupdate.com/app'
+                                '/mac/34344/alfred'),
+                        'Version': '3.4.1'},
+                       'SizeUp':
+                       {'Name': 'SizeUp',
+                        'URL': ('https://www.macupdate.com/app'
+                                '/mac/30721/sizeup'),
+                        'Version': '1.7.2'}}
+
+    @log(False)
+    def load_page_html(self, url):
+        ''' Obtain the page's HTML content '''
+
+        return requests.get(url).content
+
+    def extract_online_version(self, url):
+        ''' Extract the APP Version from MacUpdate '''
+
+        html_content = self.load_page_html(url)
+        soup = BeautifulSoup(html_content, 'lxml')
+        appVersion = soup.select('div#app-info-version-data > h4 > a')
+        appVersionStr = str(appVersion[0].get_text()).strip()
+        return appVersionStr
+
+    def get_current_version(self, appName=None):
+        ''' Extract the APP Current Version from its plist file '''
+
+        appPlistInfo = plistlib.readPlist(
+            '/Applications/{}.app/Contents/Info.plist'.format(appName))
+
+        return appPlistInfo['CFBundleShortVersionString']
+
+    def check_ver(self):
+        print('%-10s | %-10s | %-10s |' %
+              ('App', 'CurVer', 'NewVer'))
+        print('---------- | ---------- | ---------- |')
+        for app in self.appDic:
+            appName = self.appDic[app]['Name']
+            appURL = self.appDic[app]['URL']
+            appVer = self.appDic[app]['Version']
+
+            appCurrentVer = self.get_current_version(appName)
+            appOnlineVer = self.extract_online_version(appURL)
+
+            if appVer != appCurrentVer:
+                appVer = appCurrentVer
+
+            print('%-10s | %-10s | %-10s |' %
+                  (appName, appCurrentVer, appOnlineVer))
 
 
-def extract_online_version(url):
-    ''' Extract the APP Version from MacUpdate '''
-    html_content = load_page_html(url)
-    soup = BeautifulSoup(html_content, 'lxml')
-    appVersion = soup.select('div#app-info-version-data > h4 > a')
-    appVersionStr = str(appVersion[0].get_text()).strip()
-    return appVersionStr
+def main():
+    appChecker = AppChecker()
+    appChecker.check_ver()
 
 
-def get_current_version(appName=None):
-    appPlistInfo = plistlib.readPlist(
-        '/Applications/Alfred 3.app/Contents/Info.plist')
-
-    return appPlistInfo['CFBundleShortVersionString']
-
-appCurrentVer = get_current_version()
-appOnlineVer = extract_online_version(
-    'https://www.macupdate.com/app/mac/34344/alfred')
-
-print('---------- | ---------- | ---------- |')
-print('%-10s | %-10s | %-10s |' % ('Alfred', appCurrentVer, appOnlineVer))
+if __name__ == '__main__':
+    main()
